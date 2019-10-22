2cdf82de1dea2a7123580c80eda0cd116446ea9d
avitaind <52917489+avitaind@users.noreply.github.com>
2019-10-22 23:39:38 +0530

    Update diwalicampaign.blade.php

--- resources/views/emails/diwalicampaign.blade.php
+++ resources/views/emails/diwalicampaign.blade.php
@@ -6,6 +6,7 @@
 <p>Phone: {{ $phone }}</p>
 <p>Address Line 1: {{ $address1 }}</p>
 <p>Address Line 2: {{ $address2 }}</p>
+<p>Pin Code: {{ $pin }}</p>
 <p>Invoice No: {{ $invoice_no }}</p>
 <p>Invoice Image: {{ $invoice_image }}</p>
 <p>Model No: {{ $model_no }}</p>
